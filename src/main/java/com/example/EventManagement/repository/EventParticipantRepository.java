f9f9c89402c26e5c09931d1a2bd2f4ab14f3a209
hadia55op <hadizaidkhan@gmail.com>
2025-10-24 01:06:43 +0200

    made changes

--- src/main/java/com/example/EventManagement/repository/EventParticipantRepository.java
+++ src/main/java/com/example/EventManagement/repository/EventParticipantRepository.java
@@ -8,23 +8,19 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EventParticipantRepository extends JpaRepository <EventParticipant, Long> {
     List<EventParticipant> findByEvent_EventId(Long eventId);
     List<EventParticipant> findByUser_UserId(Long userId);
     List<EventParticipant> findByParticipantStatus_PStatusId(Long pStatusId);
-//    @Query("SELECT ep FROM EventParticipant ep " +
-//       "WHERE ep.user.userId = :userId " +
-//       "AND ep.participantStatus.statusName = 'Confirmed' " +
-//       "AND ep.event.startDate >= CURRENT_TIMESTAMP " +
-//       "ORDER BY ep.event.startDate ASC")
-//List<EventParticipant> findUpcomingConfirmedEventsByUser(@Param("userId") Long userId);
+
 @Query("SELECT ep FROM EventParticipant ep " +
         "WHERE ep.user.userId = :userId " +
         "AND ep.participantStatus.statusName <> 'Cancelled' " +
         "AND ep.event.startDate >= CURRENT_TIMESTAMP " +
         "ORDER BY ep.event.startDate ASC")
 List<EventParticipant> findUpcomingEventParticipants(@Param("userId") Long userId);
-
+    Optional<EventParticipant> findByUserUserIdAndEventEventId(Long userId, Long eventId);
 }
