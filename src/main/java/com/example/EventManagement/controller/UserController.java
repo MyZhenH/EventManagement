4bcef40f3f926b706a73d11603685796c1098b2c
hadia55op <hadizaidkhan@gmail.com>
2025-10-24 00:55:27 +0200

    made changes

--- src/main/java/com/example/EventManagement/controller/UserController.java
+++ src/main/java/com/example/EventManagement/controller/UserController.java
@@ -63,6 +63,18 @@ public class UserController {
 
 
     //}
+    /**
+     * This end point fetches the upcoming events for a given user.
+     * Logic explanation:
+     * - We only consider events that the user is registered for .(e.g., "Confirmed")
+     * - shows events whose start date is in the future (startDate >= CURRENT_TIMESTAMP).
+     * Result:
+     * - If the user is registered only for past events, the response will be empty.
+     * - If the user is not registered for any events at all, the response will also be empty.
+     * This is why some users (e.g., User 1) may see "No upcoming events" even though they are registered
+     * for events in the past date.
+     */
+
 
     @GetMapping("/{userId}/upcoming-events")
     public ResponseEntity<UserUpcomingEventsResponse> getUserUpcomingEvents(@PathVariable Long userId) {
