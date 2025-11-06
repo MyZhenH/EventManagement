14dc31c63b8e593aad575dfaf0c475b9b2dbc13c
Antti Keränen <24987062+aRWeeX@users.noreply.github.com>
2025-11-06 13:29:31 +0100

    fix(data): create realistic test data

--- data/create_tables.sql
+++ data/create_tables.sql
@@ -1,33 +1,33 @@
-CREATE TABLE roles(
+CREATE TABLE roles (
     role_id INTEGER PRIMARY KEY AUTOINCREMENT,
     role_name TEXT UNIQUE NOT NULL
 );
 
-CREATE TABLE categories(
+CREATE TABLE categories (
     category_id INTEGER PRIMARY KEY AUTOINCREMENT,
     category_name TEXT UNIQUE NOT NULL
 );
 
-CREATE TABLE event_statuses(
+CREATE TABLE event_statuses (
     event_status_id INTEGER PRIMARY KEY AUTOINCREMENT,
     status_name TEXT UNIQUE NOT NULL
 );
 
-CREATE TABLE participant_statuses(
+CREATE TABLE participant_statuses (
     p_status_id INTEGER PRIMARY KEY AUTOINCREMENT,
     status_name TEXT UNIQUE NOT NULL
 );
 
-CREATE TABLE users(
+CREATE TABLE users (
     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
-    phone TEXT,
+    phone TEXT UNIQUE,
     password TEXT NOT NULL,
     registration_date DATETIME,
     role_id INTEGER,
-    enabled BOOLEAN,
+    enabled BOOLEAN CHECK (enabled IN (0, 1)),
     FOREIGN KEY (role_id) REFERENCES roles (role_id)
 );
 
@@ -53,14 +53,14 @@ CREATE TABLE events (
     updated_by INTEGER,
     category_id INTEGER,
     event_status_id INTEGER,
-    FOREIGN KEY (created_by) REFERENCES users(user_id),
-    FOREIGN KEY (updated_by) REFERENCES users(user_id),
-    FOREIGN KEY (category_id) REFERENCES categories(category_id),
-    FOREIGN KEY (event_status_id) REFERENCES event_statuses(event_status_id),
-    FOREIGN KEY (city_id) REFERENCES cities(city_id)
+    FOREIGN KEY (created_by) REFERENCES users (user_id),
+    FOREIGN KEY (updated_by) REFERENCES users (user_id),
+    FOREIGN KEY (category_id) REFERENCES categories (category_id),
+    FOREIGN KEY (event_status_id) REFERENCES event_statuses (event_status_id),
+    FOREIGN KEY (city_id) REFERENCES cities (city_id)
 );
 
-CREATE TABLE event_participants(
+CREATE TABLE event_participants (
     ep_id INTEGER PRIMARY KEY AUTOINCREMENT,
     event_id INTEGER,
     user_id INTEGER,
@@ -70,11 +70,11 @@ CREATE TABLE event_participants(
     FOREIGN KEY (p_status_id) REFERENCES participant_statuses (p_status_id)
 );
 
-CREATE TABLE reviews(
+CREATE TABLE reviews (
     review_id INTEGER PRIMARY KEY AUTOINCREMENT,
     event_id INTEGER,
     user_id INTEGER,
-    rating INTEGER CHECK(rating BETWEEN 1 AND 5),
+    rating INTEGER CHECK (rating BETWEEN 1 AND 5),
     comment TEXT,
     created_at DATETIME,
     FOREIGN KEY (event_id) REFERENCES events (event_id),
